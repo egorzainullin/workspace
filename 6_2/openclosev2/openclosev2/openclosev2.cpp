2dcb6da21e7edec3c1bf0c4a110d2af235e446b8
egorzainullin <egor.zainullin@icloud.com>
2016-10-22 19:18:38 +0300

    добавлен первый коммит к : задача 6.2

---
+++ 6_2/openclosev2/openclosev2/openclosev2.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+#include "simplestack.h"
+
+using namespace std;
+
+bool isPair(StackElement *position)
+{
+	if (position && next(position) && getValue(position) == ')' && getValue(next(position)) == '(')
+	{
+		return true;
+	}
+	if (position && next(position) && getValue(position) == '}' && getValue(next(position)) == '{')
+	{
+		return true;
+	}
+	if (position && next(position) && getValue(position) == ']' && getValue(next(position)) == '[')
+	{
+		return true;
+	}
+	return false;
+}
+
+bool removePair(StackElement *position)
+{
+	if (position && (isPair(next(position))))
+	{
+		remove(position);
+		remove(position);
+		return true;
+	}
+	return false;
+}
+
+bool lastCheck(StackElement *head)
+{
+	if (!head)
+	{
+		return true;
+	}
+	if (!next(head))
+	{
+		return false;
+	}
+	if (getValue(head) == ')' && getValue(next(head)) == '(')
+	{
+		return true;
+	}
+	if (getValue(head) == '}' && getValue(next(head)) == '{')
+	{
+		return true;
+	}
+	if (getValue(head) == ']' && getValue(next(head)) == '[')
+	{
+		return true;
+	}
+	return false;
+}
+
+bool searchForPairs(Stack *stack)
+{
+	auto head = getHead(stack);
+	printReversed_r(stack);
+	auto iterator = head;
+	if ((!head) || (!next(head)) || (!next(next(head))))
+	{
+		return lastCheck(head);
+	}
+	while (iterator && (!isPair(next(iterator))))
+	{
+		if (iterator)
+		{
+			iterator = next(iterator);
+		}
+	}
+	if (removePair(iterator))
+	{
+		return searchForPairs(stack);
+	}
+	return false;
+}
+
+Stack* stringToStack(string str)
+{
+	auto stack = createStack();
+	for (int i = 0; i < str.size(); ++i)
+	{
+		if (str[i] == '(')
+		{
+			pushback(stack, '(');
+		}
+		if (str[i] == ')')
+		{
+			pushback(stack, ')');
+		}
+		if (str[i] == '{')
+		{
+			pushback(stack, '{');
+		}
+		if (str[i] == '}')
+		{
+			pushback(stack, '}');
+		}
+		if (str[i] == '[')
+		{
+			pushback(stack, '[');
+		}
+		if (str[i] == ']')
+		{
+			pushback(stack, ']');
+		}
+	}
+	return stack;
+}
+
+bool test1()
+{
+	string str1 = "{{[]}}()";
+	string str2 = "((}}";
+	auto stack1 = stringToStack(str1);
+	auto stack2 = stringToStack(str2);
+	bool testResult = searchForPairs(stack1) && !searchForPairs(stack2);
+	deleteStack(stack1);
+	deleteStack(stack2);
+	return testResult;
+}
+
+int main()
+{
+	string str = "";
+	cin >> str;
+	auto stack = stringToStack(str);
+	printReversed_r(stack);
+	cout << searchForPairs(stack) << endl;
+	deleteStack(stack);
+	cout << "launching test: " << test1() << endl;
+	return 0;
+}
---
+++ 6_2/openclosev2/openclosev2/simplestack.cpp
@@ -0,0 +1,142 @@
+#include "simplestack.h"
+#include <iostream>
+
+using namespace std;
+
+struct StackElement
+{
+	ElementType value;
+	StackElement *next;
+};
+
+struct Stack
+{
+	StackElement *head;
+};
+
+Stack* createStack()
+{
+	return new Stack{ nullptr };
+}
+
+StackElement* next(StackElement *element)
+{
+	
+	return (element) ? element->next : nullptr;
+}
+
+StackElement* getHead(Stack *stack)
+{
+	return stack->head;
+}
+
+ElementType getValue(StackElement *element)
+{
+	return element->value;
+}
+
+void deleteFromHead(Stack *stack)
+{
+	if (stack->head)
+	{
+		auto oldHead = stack->head;
+		stack->head = stack->head->next;
+		delete oldHead;
+	}
+}
+
+void deleteStack(Stack *&stack)
+{
+	while (stack->head)
+	{
+		deleteFromHead(stack);
+	}
+	delete stack;
+}
+
+void pushback(Stack *stack, ElementType value)
+{
+	auto newElement = new StackElement{ value, stack->head };
+	stack->head = newElement;
+}
+
+void insert(StackElement *position, ElementType value)
+{
+	auto newElement = new StackElement{ value, position };
+	position->next = newElement;
+}
+
+ElementType pop(Stack *stack)
+{
+	if (!stack->head)
+	{
+		auto oldElement = stack->head;
+		auto oldValue = oldElement->value;
+		stack->head = stack->head->next;
+		delete oldElement;
+		return oldValue;
+	}
+	return ' ';
+}
+
+ElementType remove(StackElement *position)
+{
+	if ((!position) || (!position->next))
+	{
+		return ' ';
+	}
+	auto oldElement = position->next;
+	auto oldValue = oldElement->value;
+	position->next = position->next->next;
+	delete oldElement;
+	return oldValue;
+}
+
+void print(Stack *stack)
+{
+	auto iterator = stack->head;
+	while (iterator)
+	{
+		cout << iterator->value << endl;
+		iterator = iterator->next;
+	}
+	cout << "===" << endl;
+}
+
+void printReversed(Stack *stack)
+{
+	auto temporaryStack = createStack();
+	auto iterator = stack->head;
+	while (iterator)
+	{
+		pushback(temporaryStack, iterator->value);
+		iterator = iterator->next;
+	}
+	print(temporaryStack);
+	deleteStack(temporaryStack);
+}
+
+void print_r(Stack *stack)
+{
+	auto iterator = stack->head;
+	while (iterator)
+	{
+		cout << iterator->value;
+		iterator = iterator->next;
+	}
+	cout << endl << "===" << endl;
+}
+
+void printReversed_r(Stack *stack)
+{
+	auto temporaryStack = createStack();
+	auto iterator = stack->head;
+	while (iterator)
+	{
+		pushback(temporaryStack, iterator->value);
+		iterator = iterator->next;
+	}
+	print_r(temporaryStack);
+	deleteStack(temporaryStack);
+}
+
