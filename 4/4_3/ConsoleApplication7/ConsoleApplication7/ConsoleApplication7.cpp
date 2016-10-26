456c4e8e74e8972cb68f525da323bc1c6caed40b
egorzainullin <egor.zainullin@icloud.com>
2016-10-26 22:06:47 +0300

    -первая рабочая версия

--- 4/4_3/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
+++ 4/4_3/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -34,8 +34,7 @@ void fileToArray(vector <string> &names, vector <string> &phones)
 
 void arrayOut(vector <string> &names, vector <string> &phones)
 {
-	int maxIndex = 0;
-	maxIndex = (names.size() < phones.size()) ? names.size() : phones.size();
+	int maxIndex = (names.size() < phones.size()) ? names.size() : phones.size();
 	for (int i = 0; i < maxIndex; ++i)
 	{
 		cout << names[i] << " " << phones[i] << endl;
@@ -44,8 +43,7 @@ void arrayOut(vector <string> &names, vector <string> &phones)
 
 void saveToFile(vector <string> &names, vector <string> &phones)
 {
-	int maxIndex = 0;
-	maxIndex = (names.size() < phones.size()) ? names.size() : phones.size();
+	int maxIndex = (names.size() < phones.size()) ? names.size() : phones.size();
 	ofstream file("file.txt");
 	for (int i = 0; i < maxIndex; ++i)
 	{
@@ -60,22 +58,84 @@ void addData(vector <string> &names, vector <string> &phones)
 	string name = "";
 	cout << "Введите имя" << endl;
 	cin >> name;
-	cout << " Введите телефон" << endl; //??????
+	cout << "Введите телефон" << endl; //??????
 	cin >> phone;
 	phones.push_back(phone);
 	names.push_back(name);
 }
 
+string findByPhone(vector <string> &names, vector <string> &phones, string request)
+{
+	int maxIndex = (names.size() < phones.size()) ? names.size() : phones.size();
+	for (int i = 0; i < maxIndex; ++i)
+	{
+		if (phones[i] == request)
+		{
+			return names[i];
+		}
+	}
+	return "Такого телефона нет в базе данных";
+}
+
+string findByName(vector <string> &names, vector <string> &phones, string request)
+{
+	int maxIndex = (names.size() < phones.size()) ? names.size() : phones.size();
+	for (int i = 0; i < maxIndex; ++i)
+	{
+		if (names[i] == request)
+		{
+			return phones[i];
+		}
+	}
+	return "Такого имени нет в базе данных";
+}
+
+bool testingComponents()
+{
+	vector <string> phones;
+	vector <string> names;
+	ofstream file1("testfile.txt");
+	file1 << "alex" << endl;
+	file1 << "5678" << endl;
+	file1 << "man" << endl;
+	file1 << "735" << endl;
+	file1 << "egor" << endl;
+	file1 << "1234" << endl;
+	string str = "";
+	string number = "";
+	file1.close();
+	ifstream file("testfile.txt", ios_base::in);
+	while (!file.eof())
+	{
+		file >> str;
+		if (file.eof())
+		{
+			break;
+		}
+		names.push_back(str);
+		file >> number;
+		if (file.eof())
+		{
+			break;
+		}
+		phones.push_back(number);
+	}
+	file.close();
+	return "egor" == findByPhone(names, phones, "1234") && "1234" == findByName(names, phones, "egor");
+}
+	
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	cout << "result of test: " << testingComponents() << endl;
 	vector <string> phones;
 	vector <string> names;
+	string request = "";
 	ifstream file("file.txt", ios_base::in);
 	if (!file.is_open())
 	{
 		file.close();
-		ofstream file("file.txt");
+		file.open("file.txt");
 	}
 	else
 	{
@@ -105,8 +165,16 @@ int main()
 			arrayOut(names, phones);
 			break;
 		case '3':
+			request = "";
+			cout << "Введите имя" << endl;
+			cin >> request;
+			cout << findByName(names, phones, request) << endl;
 			break;
 		case '4':
+			request = "";
+			cout << "Введите телефон" << endl;
+			cin >> request;
+			cout << findByPhone(names, phones, request) << endl;
 			break;
 		case '5':
 			saveToFile(names, phones);
